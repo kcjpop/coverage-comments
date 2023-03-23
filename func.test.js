1d3facce95ecc4ecc68ea022c72f6873f6061b3b
An Cao <kcj.pop.com@gmail.com>
2023-03-23 15:00:54 +0200

    More tests
    Signed-off-by: An Cao <kcj.pop.com@gmail.com>

--- func.test.js
+++ func.test.js
@@ -1,5 +1,9 @@
-const { add } = require('./func')
+const { mul, add } = require('./func')
 
 test('add', () => {
   expect(add(2, 4)).toBe(6)
+})
+
+test('mul', () => {
+  expect(mul(2, 4)).toBe(8)
 })
